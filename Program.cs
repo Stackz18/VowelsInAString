bccf7e622e91e095512fa50ef17cc03d51de158b
Anthony Stackman <Anth.stackman@hotmail.co.uk>
2019-05-10 16:26:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool active = true;
+            while (active)
+            {
+                PrintInstructions();
+                string userInput = Console.ReadLine();
+                string lowerCaseInput = userInput.ToLower();
+                if (lowerCaseInput == "exit")
+                {
+                    active = false;
+                }
+                else
+                {
+                    int vowelCount = 0;
+                    char[] charArray = lowerCaseInput.ToCharArray();
+                    for (int i = 0; i < charArray.Length; i++)
+                    {
+                        if (charArray[i] == 'a' || charArray[i] == 'e' || charArray[i] == 'i' || charArray[i] == 'o' || charArray[i] == 'u')
+                        {
+                            vowelCount++;
+                        }
+                    }
+                    Console.WriteLine("\nNumber of vowels in \"{0}\": {1}\n", userInput, vowelCount);
+                }
+            }
+        }
+
+        private static void PrintInstructions()
+        {
+            Console.WriteLine("Please type in a word and the program will check how many vowels are in the word or type exit to leave the program: ");
+        }
+    }
+}
